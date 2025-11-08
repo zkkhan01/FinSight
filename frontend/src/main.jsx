d288e77f218275ab70a78b58a35fd69d6a4e8443
Zain Khan <97347263+zkkhan01@users.noreply.github.com>
2025-11-08 15:14:05 -0600

    Refactor import statements and render method

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,6 +1,5 @@
+import React from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.jsx";
 
-import React from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-const root = createRoot(document.getElementById('root'))
-root.render(<App />)
+createRoot(document.getElementById("root")).render(<App />);
